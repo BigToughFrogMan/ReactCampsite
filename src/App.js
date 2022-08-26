aa13c55d297754118fadb45bbac08c45fd1553fd
Robert Duddy <rduddy13@gmail.com>
2022-08-26 16:54:22 -0400

    added react router paths, setting up for SPA

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import "./App.css";
 import Header from "./components/Header";
@@ -7,7 +10,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesDirectoryPage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
