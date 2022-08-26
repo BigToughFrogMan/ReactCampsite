f11a503c4a076f68d1c99e52887aaa00eef879e4
Robert Duddy <rduddy13@gmail.com>
2022-08-26 16:40:22 -0400

    added a campsite details selector, removed random selector

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -2,14 +2,18 @@ import { Col, Row } from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsites } from './campsitesSlice';
 
-const CampsitesList = () => {
+const CampsitesList = ({ setCampsiteIdFunctionAsProp }) => {
     const campsites = selectAllCampsites();
 
     return (
         <Row className='ms-auto'>
             {campsites.map((campsite) => {
                 return (
-                    <Col md='5' className='m-4' key={campsite.id}>
+                    <Col 
+                    md='5' 
+                    className='m-4' 
+                    key={campsite.id}
+                    onClick={() => setCampsiteIdFunctionAsProp(campsite.id)}>
                         <CampsiteCard campsite={campsite} />
                     </Col>
                 );
--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,6 +4,10 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-export const selectRandomCampsite = () => {
-    return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
-};
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === id);
+}
+
+// export const selectRandomCampsite = () => {
+//     return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
+// };
