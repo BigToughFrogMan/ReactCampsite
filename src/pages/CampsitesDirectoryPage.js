a1bbc846ef94e6de5db090b25209306c146a08e5
rob duddy <rduddy13@gmail.com>
2022-08-27 00:44:40 -0400

    added breadcrumbs

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,10 +1,12 @@
 import { Container } from 'reactstrap';
 import CampsitesList from '../features/campsites/CampsitesList';
+import SubHeader from '../components/SubHeader';
 
 const CampsitesDirectoryPage = () => {
 
     return (
         <Container>
+            <SubHeader current='Directory' />
             <CampsitesList />
         </Container>
     );
