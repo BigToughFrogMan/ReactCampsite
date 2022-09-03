e791b252917cbe991b17cd29996986ccbb3aa39d
Robert Duddy <rduddy13@gmail.com>
2022-09-03 03:58:17 -0400

    added the skeleton for the feedback form

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,5 +1,6 @@
 import {Container, Col, Row} from 'reactstrap';
 import SubHeader from '../components/SubHeader';
+import ContactForm from '../components/ContactForm';
 
 
 const ContactPage = () => {
@@ -41,7 +42,7 @@ const ContactPage = () => {
                     <hr />
                 </Col>
                 <Col md='10'>
-                    TBD: ContactForm
+                    <ContactForm />
                 </Col>
             </Row>
         </Container>
