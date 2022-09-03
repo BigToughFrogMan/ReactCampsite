cf9b35a27d7a512cda1ad172d16836695fd3fc7a
Robert Duddy <rduddy13@gmail.com>
2022-09-03 04:40:18 -0400

    rudimentary form validation added

---
+++ src/utils/validateContactForm.js
@@ -0,0 +1,34 @@
+export const validateContactForm = (values) => {
+    const errors = {};
+
+    if (!values.firstName) {
+        errors.firstName = 'Required';
+    }
+    else if (values.firstName.length < 2) {
+        errors.firstName = 'Must be at least 2 characters.';
+    }
+    else if (values.firstName.length > 15) {
+        errors.firstName = 'Must be 15 characters or less';
+    }
+
+    if (!values.lastName) {
+        errors.lastName = 'Required';
+    }
+    else if (values.lastName.length < 2) {
+        errors.lastName = 'Must be at least 2 characters.';
+    }
+    else if (values.lastName.length > 15) {
+        errors.lastName = 'Must be 15 characters or less';
+    }
+
+    const reg = /^[\d+]{9}$/;
+    if (!reg.test(values.phoneNum)) {
+        errors.phoneNum = 'The phone number should contain nine numbers and no other characters.';
+    }
+
+    if (!values.email.includes('@')) {
+        errors.email = 'Email should contain a @';
+    }
+
+    return errors;
+};
