b04cb5c3398ef9c3f40114ff76a26327ad408e06
Robert Duddy <rduddy13@gmail.com>
2022-09-17 09:56:46 -0400

    async campsites

--- src/App.js
+++ src/App.js
@@ -7,8 +7,17 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import AboutPage from './pages/AboutPage';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+}, [dispatch]);
+
   return (
     <div className="App">
       <Header />
